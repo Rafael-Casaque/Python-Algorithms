3a13b9384833dd412567db1021ca311b6f7259dc
rafael rocha <rrcasaque@hotmail.com>
2021-08-13 22:05:03 -0300

    first upload

---
+++ simple calculator (CLI).py
@@ -0,0 +1,23 @@
+nome = input('digite o nome: ')
+sobrenome = input('digite o sobrenome: ')
+print ('Usuário: ',nome+ ' ' + sobrenome)
+print('\n')
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+
+operation = str(input('Digite a operação desejada: '))
+
+if operation == "soma" or operation == "SOMA":
+    n3 = n1+n2
+elif operation == "subtração" or operation == "SUBTRAÇÃO":
+    n3 = n1-n2
+elif operation == "multiplicação" or operation == "MULTIPLICAÇÃO":
+    n3 = n1*n2
+elif operation == "divisão" or operation == "DIVISÃO":
+    n3 = n1/n2
+else:
+    print('Erro, você não digitou a operação corretamente!')
+
+print('\nA operação de', operation, 'entre ',n1,' e ',n2, 'é igual a: ',n3)
+print('\n\nDigite qualquer tecla para fechar o programa')
+input()
